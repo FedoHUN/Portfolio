50f38d4b1d984943cf2d735457c3854d617c14b5
Vojtko Máté <fedohun@gmail.com>
2023-12-13 10:51:29 +0100

    vmc

--- portfolio/src/containers/projects/Projects.jsx
+++ portfolio/src/containers/projects/Projects.jsx
@@ -1,9 +1,10 @@
 import React, {useEffect} from 'react';
 import './projects.css';
 import page1 from '../../assets/GPT3.png'
-import gradient from '../../assets/gradient_img.png'
+import gradient from '../../assets/gradient_img.png';
 import { useInView } from "react-intersection-observer";
 import { animate, motion, useAnimation } from "framer-motion";
+import mvcredit from '../../assets/mvcredit.png';
 
 function Section({ children }) {
   const controls = useAnimation();
@@ -44,8 +45,18 @@ const Blog = () => {
       <Section>
         <div className='pf__projects-container'>
           <div className='pf__projects-container_article first-article'>
-          <a href='https://fedohun.github.io/mgpt-3/' target='_blank' className='mgpt-3__anchor'>
-              <img src={page1} alt='GPT3-Site' />            <span></span>
+          <a href='https://fedohun.github.io/vmcredit' target='_blank' className='mgpt-3__anchor'>
+              <img src={mvcredit} alt='mvcredit' />            <span></span>
+            <div className='pf__projects-container_article-content'>
+              <h3>MVCredit</h3>
+              <p>Open Page</p>
+            </div>
+            </a>
+          </div>
+          <div className='pf__projects-container_article'>
+              <a href='https://fedohun.github.io/mgpt-3/' target='_blank' className='mgpt-3__anchor'>
+              <img src={page1} alt='gpt' />
+              <span></span>
             <div className='pf__projects-container_article-content'>
               <h3>GPT-3 Page</h3>
               <p>Open Page</p>
@@ -53,19 +64,8 @@ const Blog = () => {
             </a>
           </div>
           <div className='pf__projects-container_article'>
-              <img src={gradient} alt='gradient' />
-              <div className='coming'>
-                <h1>Coming Soon...</h1>
-              </div>
-              <span></span>
-            <div className='pf__projects-container_article-content'>
-              <h3>Next Project</h3>
-              <p>Coming Soon...</p>
-            </div>
-          </div>
-          <div className='pf__projects-container_article'>
             <div className='pf__projects-container_article-image'>
-              <img src={gradient} alt='gradient' />
+              <img src={gradient} alt='coming' />
               <div className='coming'>
                 <h1>Coming Soon...</h1>
               </div>
